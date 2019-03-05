c1f0ea97344063808ac2438e618bd32df614f674
trozio <daniel.l.adelman@gmail.com>
2019-03-05 18:43:54 -0500

    mysql2, sequelize, and placeholder connection

---
+++ connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+});
