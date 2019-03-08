5c9cf58481461f38fb7bfbd7a03afcd550dc81ab
trozio <daniel.l.adelman@gmail.com>
2019-03-07 21:08:03 -0500

    updated index.html

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,14 @@
-let express = require("express");
-let app = express();
 let path = require("path");
-
 module.exports = function(app) {
-	app.get("/", function(req, res) {
+  app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/login.html"));
+  })
+  app.get("/index.html", function(req, res){
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
 
-		res.sendFile(path.join(__dirname, "../public/index.html"));
-	});
+  app.post("/api/users", function(req, res){
+let newUser = req.body;
+  })
 
-
-}
+};
