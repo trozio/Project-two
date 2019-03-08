0fcebf8da04ef45274458a3bca1056ef5e0a9aec
trozio <daniel.l.adelman@gmail.com>
2019-03-07 21:28:47 -0500

    css

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./routes/htmlRoutes.js")(app);
 
