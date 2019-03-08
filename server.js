af35878e75b6c3f5bde7c0223b3989d2531a5462
trozio <daniel.l.adelman@gmail.com>
2019-03-07 19:30:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,17 +10,6 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
