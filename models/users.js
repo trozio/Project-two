3ecf88dc6a6bf428290a6615363b8a6c1d18c18f
Chris Offiong <chrisoffiong@gmail.com>
2019-03-12 01:19:13 -0400

    revised

---
+++ models/users.js
@@ -0,0 +1,3 @@
+let usersArr = []
+
+module.exports = usersArr
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 let path = require("path");
+const User = require('../models/users.js');
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
@@ -15,5 +16,6 @@ module.exports = function(app) {
   app.get("/profile/:name", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/profile.html"))
     res.send(req.params)
+    console.log(req.params)
   })
 }
