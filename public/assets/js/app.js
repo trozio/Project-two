c6f72fadf673405b85f00a1d8fbf8e208cfb4ed6
trozio <daniel.l.adelman@gmail.com>
2019-03-12 20:11:30 -0400

    token js

---
+++ public/assets/js/app.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function () {
+
+    var idToken;
+    var accessToken;
+    var expiresAt;
+
+    var webAuth = new auth0.WebAuth({
+        domain: 'chrisoffiong.auth0.com',
+        clientID: 'hkwpOqxy86BQtd8MpVH8wpRNNw08R1FN',
+        responseType: 'token id_token',
+        scope: 'openid profile',
+        redirectUri: 'http://localhost:3000/api/profile'
+    });
+
+    // ...
+    var loginBtn = document.getElementById('button');
+
+    loginBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        webAuth.authorize();
+    });
+
+})
