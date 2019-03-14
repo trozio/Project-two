d1ef3107c5f010ae41dc2e13c74708f3da7fae52
trozio <daniel.l.adelman@gmail.com>
2019-03-13 20:17:53 -0400

    Syntax cleaning

--- public/assets/js/token.js
+++ public/assets/js/token.js
@@ -1,37 +1,24 @@
-var apiUrl = 'http://chrisoffiong.auth0.com/userinfo';
-var token = window.location.hash;
+let apiUrl = 'http://chrisoffiong.auth0.com/userinfo';
+let token = window.location.hash;
 
 function convertToken(token) {
-    let tokenObj = {};
-    let hashToken = token.slice(1).split('&');
-
-    for (let i = 0; i < hashToken.length; i++) {
-        let subResult = hashToken[i].split('=');
-        for (let j = 0; j < subResult.length; j++) {
-            tokenObj[subResult[0]] = subResult[1];
-        }
-    }
-    return tokenObj;
+	let tokenObj = {};
+	let hashToken = token.slice(1).split('&');
+	for (let i = 0; i < hashToken.length; i++) {
+		let subResult = hashToken[i].split('=');
+		for (let j = 0; j < subResult.length; j++) {
+			tokenObj[subResult[0]] = subResult[1];
+		}
+	}
+	return tokenObj;
 }
 
-let result = convertToken(token);
+let convertedToken = convertToken(token);
 
-function callAPI() {
-
-    $.ajax({
-        type: "POST",
-        url: '/get/token',
-        data: result
-    }).then(function (data) {
-        $('.container').html(`
-            <p>My Name is ${data.nickname}</p>
-            <img src="${data.picture}" />
-            <p>Email: ${data.name}</p>
-
-        `);
-    })
-
-    }
-
-
-callAPI();
+$.ajax({
+	type: "POST",
+	url: '/get/token',
+	data: convertedToken
+}).then(function(data) {
+	// $('.container').html(`<p>My Name is ${data.nickname}</p><img src="${data.picture}" /><p>Email: ${data.name}</p>`);
+});
