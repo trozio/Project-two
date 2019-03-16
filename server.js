cb002d9df50b6e510454ecd0004a71189fce749a
Chris Offiong <chrisoffiong@gmail.com>
2019-03-16 04:48:55 -0400

    revised

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'))
-app.use('/static', express.static(__dirname + '/public'));
 app.use(express.static('public'))
 
 
