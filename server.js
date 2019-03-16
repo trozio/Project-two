f1d59b379a5004acd966caf3279b9cfa884cbc99
Chris Offiong <chrisoffiong@gmail.com>
2019-03-16 04:50:51 -0400

    revised

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use("/public", express.static('./public/'))
 
 
 require("./routes/htmlroutes.js")(app);
